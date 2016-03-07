7c6d56d2b27591161d7bd17888c935ddcb5d7831
jorge nieto <jorge.nieto@babel.es>
2016-03-07 13:09:03 +0100

    Bucle tercera version

--- bucle_asincrono.js
+++ bucle_asincrono.js
@@ -9,18 +9,17 @@ var escribeTras2Segundos = function(texto, callback) {
    }, 500);
 };
 
-function serie(i, fn){
-	fn('texto' + i, function(){
-		i--;
-		
-	    if (i == 0) {
-	    	console.log('fin');
-	    	return;
-		}
+function serie(arr, fn){
+	
+	if(arr.length == 0){
+		console.log('fin');
+	    return;
+	}
 
-		serie(i, fn);
+	fn(arr.shift(), function(){
+		serie(arr, fn);
 	});
 	
 } 
 //solo le doy la funcion de escribeTras2Segundos
-serie(5, escribeTras2Segundos);
+serie([1,2,3,4,5], escribeTras2Segundos);

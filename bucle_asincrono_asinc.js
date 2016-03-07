6b6c393dea317c4f99139d225bd793b3ac9bb7b9
jorge nieto <jorge.nieto@babel.es>
2016-03-07 13:27:48 +0100

    Bucle asincrono asinc 4.1 con la division es par y sale a error

--- bucle_asincrono_asinc.js
+++ bucle_asincrono_asinc.js
@@ -15,7 +15,10 @@ var escribeTras2Segundos = function(texto, callbackTras2Segundos) {
 async.eachSeries([1,2,3,4,5], 
 	function cadaElemento(item, callbackSiguiente){
 		escribeTras2Segundos(item, function(){
-			callbackSiguiente();
+			//si la division de item /2 es nulo o si es error
+			//al llegar a 2 como la division es par va a error y termina
+			var res = item % 2 ? null : 'error';
+			callbackSiguiente(res);
 		})
 	},function fin(){
 		console.log('fin');

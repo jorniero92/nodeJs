b0a1949ce447b65e7f5df542ae4da5f64c7da1d9
jorge nieto <jorge.nieto@babel.es>
2016-03-08 13:27:54 +0100

    herencias dobles

--- prototipo.js
+++ prototipo.js
@@ -1,5 +1,7 @@
 "use strict";
 
+
+
 // Hacer funcion constructora
 var Persona = function(name){
 	this.name = name;
@@ -20,3 +22,42 @@ luis.saluda();
 var pepe = new Persona('pepe');
 
 pepe.saluda();
+
+// Herencia ----
+// Otra funcin que hereda de persona
+var Agente = function(name){
+    //Ejecutamos constructor heredado
+    //Lo mismo que llamar a super() en otros lenguajes
+    Persona.call(this, name); //Bindeamos nombre
+}
+
+Agente.prototype = new Persona();
+
+var juan = new Agente("Juan");
+
+juan.saluda();
+
+
+console.log(Object.getPrototypeOf(juan));
+console.log(juan instanceof Agente);
+console.log(juan instanceof Persona);
+console.log(juan instanceof Object);
+
+//ejemplo de herencia de eventEmiter
+var EventEmiter = require('events').EventEmitter;
+var extend = require('util')._extend;
+
+
+Persona.prototype = extend(Persona.prototype, EventEmiter.prototype);
+
+var jose = new Persona('jose');
+
+jose.on('asdas', function(data){
+	console.log('ha saltado evento!', data);
+});
+
+jose.emit('asdas', { playload: 55000});
+
+process.on('exit', function(){
+	jose.emit('asdas', 'el programa termina!');
+});

a0a4ced0a126c6214a4c9138a5693e4e97b11e7d
jorge nieto <jorge.nieto@babel.es>
2016-03-11 12:19:03 +0100

    Ejemplo de promesa basica 1

---
+++ promise.js
@@ -0,0 +1,35 @@
+"use strict";
+/*
+* como voy a devolver una promesa no hace falta callback
+var sleep = function(ms,callback){}
+*/
+var sleep = function(ms){
+	//console.log(ms);
+	//creamos la promesa
+	var promesa = new Promise(function(resolve, reject){
+	// antes del setTimeout creamos la promesa
+		setTimeout(function(){
+			if(ms % 2 !== 0){ // si es par los ms
+				reject(new Error('Fatal'));
+				return;
+			}
+			//cuando pasa el tiempo resolvemos la promesa
+			resolve({name: 'Jones'});
+			return;	
+		}, ms);
+
+	});
+	return promesa;
+};
+
+//no hay problema xq la otra variable esta dentro de una funcion
+var promesa = sleep(1000);
+
+console.log(promesa);
+
+promesa.then(sleep)
+	.then(function(){
+	console.log('promesa terminada!');
+}).catch(function(err){
+	console.error('Error fatal!', err);
+});

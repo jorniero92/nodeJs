6b4d929f48ad94263cc0fecb6e2fc65ce9a8d4d8
jorge nieto <jorge.nieto@babel.es>
2016-03-11 12:31:30 +0100

    Ejemplo de promesas, funcion paella no lleva ajo

--- promise2.js
+++ promise2.js
@@ -1,6 +1,38 @@
 "use strict";
-function arroz(plato){
+
+function conArroz(plato){
 	return new Promise(function(resolve, reject){
-		resolve(plato + 'Arroz');
+		resolve(plato + ' Arroz');
 	})
 }
+
+function conSal(plato){
+	return new Promise(function(resolve, reject){
+		resolve(plato + ' Sal');
+	})
+}
+
+function conAjo(plato){
+	return new Promise(function(resolve, reject){
+		reject('No lleva Ajo la paella');
+	})
+}
+
+function con(plato){
+	return new Promise(function(resolve, reject){
+		resolve(plato + '' + ing);
+	})
+}
+
+var paella = 'paella con';
+
+conArroz(paella) //cuando se devuelva conArroz saca un string
+//conArroz('') //cuando se devuelva conArroz saca un string
+	.then(conAjo)
+	.then(conSal) // esuna funcion que resuelve
+	.then(function(plato){
+		console.log('El plato tiene: ', plato);
+	})
+	.catch(function(err){
+		console.log('Error', err);
+	});
